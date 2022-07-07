ecfa30e076475d7430977cbbfb81bc58102f4473
Mario Steele <mario@ruby-im.net>
2022-07-07 03:08:06 -0500

    Added missing documentation to functions and Attributes

--- lib/Classes/ResourceAttribute.cs
+++ lib/Classes/ResourceAttribute.cs
@@ -3,11 +3,18 @@ using Godot;
 
 namespace Godot.Sharp.Extras {
 
+	/// <summary>
+	/// Sets up a Resource to be loaded into variable given.
+	/// </summary>
 	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
 	public class ResourceAttribute : Attribute
 	{
 		public string ResourcePath { get; set; }
 
+		/// <summary>
+		/// Constructs a <see cref="ResourceAttribute"/>
+		/// </summary>
+		/// <param name="path">The path to the Resource to load into the variable.</param>
 		public ResourceAttribute(string path) {
 			this.ResourcePath = path;
 		}
--- lib/Classes/SingletonAttribute.cs
+++ lib/Classes/SingletonAttribute.cs
@@ -5,11 +5,20 @@ using System.Threading.Tasks;
 
 namespace Godot.Sharp.Extras
 {
+	/// <summary>
+	/// Sets up a Singleton/Autoload to be loaded into the Given Variable.  This Singleton needs to be added in
+	/// the Godot Editor, in order for it to be properly loaded into the SceneTree and fetched by this
+	/// attribute.
+	/// </summary>
 	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
 	public class SingletonAttribute : Attribute
 	{
 		public string Name { get; set; }
 
+		/// <summary>
+		/// Constructs a <see cref="SingletonAttribute"/>
+		/// </summary>
+		/// <param name="path">Optional Name / Path to the Singleton</param>
 		public SingletonAttribute(string path = "") {
 			this.Name = path;
 		}
